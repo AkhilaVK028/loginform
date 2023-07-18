ffd540179be656824635698b8b99b249a9e75293
akhila.vk <akhilavk028@gmail.com>
2023-07-18 11:23:12 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,60 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+ import { LoginComponent }  from './login/login.component';
+import { NZ_I18N, } from 'ng-zorro-antd/i18n';
+import {en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzImageModule } from 'ng-zorro-antd/image';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+
+registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+  AppComponent,
+  // logincomponent
+  LoginComponent
+ ],
+ imports: [
+   BrowserModule,
+   AppRoutingModule,
+   FormsModule,
+   HttpClientModule,
+   BrowserAnimationsModule,
+   IconsProviderModule,
+   NzLayoutModule,
+   NzMenuModule,
+   NzButtonModule ,
+   NzFormModule,
+   ReactiveFormsModule,
+   NzSelectModule ,
+   NzInputModule ,
+   NzCheckboxModule ,
+   NzGridModule ,
+   NzDatePickerModule,
+   NzImageModule,
+   NzRadioModule 
+   
+
+ ],
+ providers: [
+{ provide: NZ_I18N, useValue: en_US }
+],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
